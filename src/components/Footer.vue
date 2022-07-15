1f45707568a48a5f790240a07232ec4ac93249fd
Crisano97 <97keb6@gmail.com>
2022-07-15 18:05:24 +0200

    footer bottom completed

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,13 +1,22 @@
 <template>
   <footer>
-    <div class="footer-top">
+    <div class="footer-top d-flex justify-content-around">
         <nav>
             
         </nav>
     </div>
-    <div class="footer-bottom d-flex justify-content-around">
-        <div class="sign-up"></div>
-        <div class="follow"></div>
+    <div class="footer-bottom d-flex justify-content-between">
+        <button class="sign-up">SIGN-UP NOW!</button>
+        <div class="follow d-flex align-items-center justify-content-center">
+            <span>FOLLOW US</span>
+            <div class="img-wrapper d-flex justify-content-around w-50 ms-3">
+                <img src="../assets/images/footer-facebook.png" alt="">
+                <img src="../assets/images/footer-twitter.png" alt="">
+                <img src="../assets/images/footer-youtube.png" alt="">
+                <img src="../assets/images/footer-pinterest.png" alt="">
+                <img src="../assets/images/footer-periscope.png" alt="">
+            </div>
+        </div>
     </div>
   </footer>
 </template>
@@ -18,36 +27,49 @@ export default {
 }
 </script>
 
-<style  lang="scss">
+<style  lang="scss" scoped>
 
-    div.footer-top{
+   footer{
+
+        background-image: url('../assets/images/footer-bg.jpg');
+        background-size: cover;
+
+     div.footer-top{
         width: 100%;
-        background-color: gray;
+            height: 25.2rem;
 
+        
         nav{
-            background-color: cadetblue;
-            height: 24.3rem;
-            width: 25rem;
-            margin-left: 17.3rem;
+            width: 75%;
+            background-image: url('../assets/images/dc-logo-bg.png');
+            background-repeat: no-repeat;
+            background-position: right;
         }
     }
 
     div.footer-bottom{
-        padding: 1.3rem 2rem;
+        padding: 1.3rem 17rem;
         width: 100%;
-        background-color: lightgrey;
+        background-color: #303030;
 
-        div.sign-up{
-            height: 4rem;
-            width: 10rem;
-            background-color: aqua;
+        button.sign-up{
+            padding: 10px 10px;
+            background-color: transparent;
+            border-color: #0282F9;
+            color: white;
         }
 
         div.follow{
-            height: 4rem;
-            width: 45rem;
-            background-color: beige;
+            // height: 4rem;
+            width: 28rem;
+
+            span{
+                color: #0282F9;
+                font-weight: bold;
+                font-size: 1.3rem;
+            }
         }
     }
+   }
 
 </style>
