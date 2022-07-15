9eb964cafe3af49f91f4aa4c0eaaa53899968e3f
Crisano97 <97keb6@gmail.com>
2022-07-15 15:53:34 +0200

    general block structure

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,10 @@
 <template>
   <footer>
-    <div class="footer-top"></div>
+    <div class="footer-top">
+        <nav>
+            
+        </nav>
+    </div>
     <div class="footer-bottom d-flex justify-content-around">
         <div class="sign-up"></div>
         <div class="follow"></div>
@@ -18,24 +22,30 @@ export default {
 
     div.footer-top{
         width: 100%;
-        height: 25rem;
         background-color: gray;
+
+        nav{
+            background-color: cadetblue;
+            height: 24.3rem;
+            width: 25rem;
+            margin-left: 17.3rem;
+        }
     }
+
     div.footer-bottom{
-        padding: 1.3rem;
+        padding: 1.3rem 2rem;
         width: 100%;
-        height: 7.5rem;
         background-color: lightgrey;
 
         div.sign-up{
-            height: 5rem;
-            width: 5rem;
+            height: 4rem;
+            width: 10rem;
             background-color: aqua;
         }
 
         div.follow{
-            height: 5rem;
-            width: 50rem;
+            height: 4rem;
+            width: 45rem;
             background-color: beige;
         }
     }
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,7 +1,17 @@
 <template>
   <main>
-    <div class="content"></div>
-    <div class="main-nav"></div>
+    <div class="content">
+        <span>-->Content goes here</span>
+    </div>
+    <div class="main-nav">
+        <div class="link-wrapper d-flex justify-content-between p-4">
+            <div class="link"></div>
+            <div class="link"></div>
+            <div class="link"></div>
+            <div class="link"></div>
+            <div class="link"></div>
+        </div>
+    </div>
   </main>
 </template>
 
@@ -16,13 +26,27 @@ export default {
 
     div{
         width: 100%;
-        height: 8rem;
+        // height: 8rem;
     }
 
     div.content {
         background-color: #1C1C1C;
+        color: white;
+        padding: 3rem 17.3rem;
+
+        span{
+            font-size: 1.7rem;
+            font-weight: bold;
+        }
     }
     div.main-nav{
         background-color: $mainBgColor;
+        padding: 1rem 17rem;
+
+        div.link{
+            height: 4rem;
+            width: 10rem;
+            background-color: gray;
+        }
     }
 </style>
